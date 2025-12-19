89c68be95cf1195131245f72b078fc00e6384e8a
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2025-12-18 22:43:55 -0300

    feat: cadastrar hash da senha do usuário no banco e remover retorno da senha no enpoint de criação

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -6,9 +7,22 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(data: CreateUserDto) {
+  async create(data: CreateUserDto) {
+    const saltRounds = 10;
+
+    const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
     return this.prisma.user.create({
-      data,
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
     });
   }
 }
