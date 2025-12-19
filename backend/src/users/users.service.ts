b2195dea8b7fe1a89f894a9ed07901709c1cf5b1
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2025-12-18 21:58:19 -0300

    feat: adicionar endpoint de criação de usuário

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: CreateUserDto) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+}
